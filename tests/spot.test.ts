4d69d4abf31053411b535b6f6cf6fcd73e9af0d3
Arkadiusz Mastalerczuk <arekmas@gmail.com>
2022-07-07 12:20:02 +0200

    Added tests of SpotRecord.getAll method.

--- tests/spot.test.ts
+++ tests/spot.test.ts
@@ -1,6 +1,12 @@
 import {SpotRecord} from '../records/spot.record';
+import {pool} from '../utils/db';
+import {SpotEntity} from '../types';
 
 /* eslint-disable no-undef */
+afterAll(async () => {
+    await pool.end();
+});
+
 test('SpotRecord returns data from database for one entry.', async () => {
     const spot = await SpotRecord.getOne('exampleId');
 
@@ -16,9 +22,39 @@ test('SpotRecord returns data from database for one entry.', async () => {
     expect(spot.instagramUrl).toBeNull();
 });
 
-test('SpotRecord returns null from database for unexisting entry.', async () => {
+test('SpotRecord.getOne returns null from database for unexisting entry.', async () => {
     const spot = await SpotRecord.getOne('noId');
     expect(spot).toBeNull();
 });
 
+test('SpotRecord.getAll returns array of found entries.', async () => {
+    const spots = await SpotRecord.getAll('');
+    expect(spots).not.toEqual([]);
+    expect(spots[0]).toBeDefined();
+});
+
+test('SpotRecord.getAll returns an empty array when searching for something that not exists.', async () => {
+    const spots = await SpotRecord.getAll('qwertyuiop asdfghjkl zaq12wsx');
+    expect(spots).toEqual([]);
+});
+
+test('SpotRecord.getAll returns not all data but only: id, latitude and longitude', async () => {
+    const spots = await SpotRecord.getAll('');
+    expect(spots[0].id).toBeDefined();
+    expect(spots[0].latitude).toBeDefined();
+    expect(spots[0].longitude).toBeDefined();
+    expect((spots[0] as SpotEntity).name).toBeUndefined();
+    expect((spots[0] as SpotEntity).description).toBeUndefined();
+    expect((spots[0] as SpotEntity).image).toBeUndefined();
+    expect((spots[0] as SpotEntity).siteUrl).toBeUndefined();
+    expect((spots[0] as SpotEntity).facebookUrl).toBeUndefined();
+    expect((spots[0] as SpotEntity).youtubeUrl).toBeUndefined();
+    expect((spots[0] as SpotEntity).instagramUrl).toBeUndefined();
+});
+
+test('SpotRecord.getAll returns array of found entries when searching for "ryn".', async () => {
+    const spots = await SpotRecord.getAll('ryn');
+    expect(spots).not.toEqual([]);
+    expect(spots[0].id).toBeDefined();
+});
 /* eslint-enables no-undef */
