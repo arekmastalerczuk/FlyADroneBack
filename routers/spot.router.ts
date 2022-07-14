e708c896dc29c37162405c2a68663337f1cbb1c7
Arkadiusz Mastalerczuk <arekmas@gmail.com>
2022-07-14 21:36:58 +0200

    Added multer to project to handle uploading files from frontend, limits for type and size of files have been set.

--- routers/spot.router.ts
+++ routers/spot.router.ts
@@ -1,7 +1,33 @@
-import {Router} from 'express';
+import {Request, Router} from 'express';
+import multer from 'multer';
 import {SpotRecord} from '../records/spot.record';
 
 export const spotRouter = Router();
+
+const fileStorageEngine = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, './images');
+    },
+    filename: (req, file, cb) => {
+        cb(null, `${Date.now()}-${file.originalname}`);
+    },
+});
+
+const fileFilter = (req: Request, file: any, cb: any) => {
+    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/jpg' || file.mimetype === 'image/png') {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+};
+
+const upload = multer({
+    storage: fileStorageEngine,
+    limits: {
+        fileSize: 1024 * 1024 * 10, // limit 10MB
+    },
+    fileFilter,
+});
 
 spotRouter
     .get('/search/:spotAddress?', async (req, res) => {
@@ -14,7 +40,14 @@ spotRouter
         res.json(spot);
     })
 
-    .post('/', async (req, res) => {
+    // .post('/', async (req, res) => {
+    //     const spot = new SpotRecord(req.body);
+    //     await spot.insert();
+    //     res.json(spot);
+    // });
+
+    .post('/', upload.single('image'), async (req, res, next) => {
+        // console.log(req.file);
         const spot = new SpotRecord(req.body);
         await spot.insert();
         res.json(spot);
